c6070dda20ccf51cd4a94759818300aa02354faf
overseeas <overseeas@gmail.com>
2024-05-30 17:41:13 +0900

    Renamed module, renamed methods

--- tokko.py
+++ tokko.py
@@ -1,13 +1,13 @@
 from robocorp import windows
 
-def login_tokko(path, id, password):
+def login_tokko(id, password):
     desktop = windows.desktop()
-    desktop.windows_run(path)
-    login = vault.get_secret("tokko")
+    desktop.windows_run("C:\Program Files (x86)\TokkoTencho\特攻店長\特攻店長.exe")
     tokko_login = windows.find_window('subname:"特攻店長 - ログイン"')
     tokko_login.find("id:txtUserID").set_value(id)
     tokko_login.find("id:txtPassword").set_value(password, validator = None)
     tokko_login.find("id:btnLogin").click()
+    assert windows.find_window('subname:"特攻店長 - メインメニュー"')
 
 def select_menu(menu):
     tokko_main = windows.find_window('subname:"特攻店長 - メインメニュー"')
@@ -32,30 +32,31 @@ def select_menu(menu):
     }
     tokko_main.find(menu_dict[menu]).click()
 
-def set_search_config():
+def initialize_order_config():
     tokko_order = windows.find_window('subname:"特攻店長 - 受注一覧"')
     #initialize status
     tokko_order.select("", locator="id:cmbStatus")
-
     #詳細検索
-    assert tokko_order.find("name:詳細検索▼").click()
-    detailed_options = tokko_order.find_many("id:cmbSearchType", search_strategy="all", search_depth=6)
-    detailed_options = detailed_options[:-1]
+    if tokko_order.find_many("name:詳細検索▼"):
+        tokko_order.find("name:詳細検索▼").click()
+    
+    #detailed_options = tokko_order.find_many("id:cmbSearchType", search_strategy="all", search_depth=6)
+    #detailed_options = detailed_options[:-1]
 
-def option_text(index, name, value, contain = "を含む"):
+def option_text(index: int, option_name: str, input_text: str, contain = "を含む"):
     tokko_order = windows.find_window('subname:"特攻店長 - 受注一覧"')
     detailed_options = tokko_order.find_many("id:cmbSearchType", search_strategy="all", search_depth=6)
     detailed_options = detailed_options[:-1]
-    detailed_options[index].select(name)
+    detailed_options[index].select(option_name)
     option_panel = detailed_options[index].get_parent()
-    option_panel.set_value(value, locator="id:txtValue")
+    option_panel.set_value(input_text, locator="id:txtValue")
     option_panel.select(contain, locator="id:cmbSearchCondition")
 
-def option_list(index, name, value, contain = "を含む"):
+def option_list(index: int, option_name: str, input_option: str, contain = "を含む"):
     tokko_order = windows.find_window('subname:"特攻店長 - 受注一覧"')
     detailed_options = tokko_order.find_many("id:cmbSearchType", search_strategy="all", search_depth=6)
     detailed_options = detailed_options[:-1]
-    detailed_options[index].select(name)
+    detailed_options[index].select(option_name)
     option_panel = detailed_options[index].get_parent()
-    option_panel.select(value, locator="id:cmbValue")
+    option_panel.select(input_option, locator="id:cmbValue")
     option_panel.select(contain, locator="id:cmbSearchCondition")
